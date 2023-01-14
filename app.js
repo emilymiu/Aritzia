b768f06e0776ede7eb351ba2de15c230e2987dc5
vihangiperera <vihangip@students.ubc.ca>
2023-01-13 22:16:26 -0800

    added rest

---
+++ app.js
@@ -0,0 +1,23 @@
+document.getElementById("hello-button").addEventListener("click", printHello);
+
+function printHello() {
+  console.log("Hello");
+  document.getElementById("hello").innerHTML = "Hello!";
+}
+
+let count = 0;
+
+function addOne() {
+  count++;
+  document.getElementById("counter").innerHTML = count;
+}
+
+document.getElementById("counter-button").addEventListener("click", addOne);
+
+function printWatermelon(event) {
+  document.getElementById("watermelon-text").innerHTML = event.target.value;
+}
+
+document
+  .getElementById("watermelon-button")
+  .addEventListener("click", printWatermelon);
